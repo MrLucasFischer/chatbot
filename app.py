8cc382f00e0a761393ebb88bf2e79953c858f3b0
MrLucasFischer <lucasfischerpt@gmail.com>
2019-02-08 16:32:45 +0000

    added bert reranking

--- app.py
+++ app.py
@@ -58,11 +58,11 @@ def webhook():
 def send_message(recipient_id, user_message):
 
     lucene_response = get_lucene_response(user_query=user_message) #Get a response from lucene
-    log("MESSAGE FROM LUCENE: "+lucene_response)
+    # log("MESSAGE FROM LUCENE: "+lucene_response)
 
     bert_response = bert_rerank(user_message, lucene_response)
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=bert_response))
+    # log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=bert_response))
 
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
